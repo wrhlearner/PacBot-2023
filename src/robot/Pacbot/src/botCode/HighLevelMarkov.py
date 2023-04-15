2753a014a89b65bf716a95cb6d8105d80f2e7d24
CrAZYGreenAlpaca <khouiderines@gmail.com>
2023-04-15 16:54:38 -0400

    added high level markov, still need to edit is dead and make sure states

---
+++ src/robot/Pacbot/src/botCode/HighLevelMarkov.py
@@ -0,0 +1,18 @@
+import os, copy
+import robomodules as rm
+from variables import *
+from grid import grid
+from search import bfs
+from messages import MsgType, message_buffers, LightState, PacmanCommand
+
+ADDRESS = os.environ.get("LOCAL_ADDRESS","localhost")
+PORT = os.environ.get("LOCAL_PORT", 11295)
+
+FREQUENCY = 60
+
+class HighLevelMarkov(rm.ProtoModule):
+    def __init__(self, addr, port):
+        self.subscriptions = [MsgType.LIGHT_STATE]
+        super().__init__(addr, port, message_buffers, MsgType, FREQUENCY, self.subscriptions)
+        self.state = None
+        self.grid = copy.deepcopy(grid)
